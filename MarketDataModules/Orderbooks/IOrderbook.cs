81e0749afdb53f72dd61213f2b4566c223cd6c45
BlankaKorvo <BlankaKorvo@gmail.com>
2022-04-12 11:54:12 +0300

    delete interface

--- MarketDataModules/Orderbooks/IOrderbook.cs
+++ MarketDataModules/Orderbooks/IOrderbook.cs
@@ -1,12 +1,12 @@
-﻿using System.Collections.Generic;
+﻿//using System.Collections.Generic;
 
-namespace MarketDataModules.Orderbooks
-{
-    public interface IOrderbook
-    {
-        public int Depth { get; }
-        public List<OrderbookEntry> Bids { get; }
-        public List<OrderbookEntry> Asks { get; }
-        public string Figi { get; }
-    }
-}
+//namespace MarketDataModules.Orderbooks
+//{
+//    public interface IOrderbook
+//    {
+//        public int Depth { get; }
+//        public List<OrderbookEntry> Bids { get; }
+//        public List<OrderbookEntry> Asks { get; }
+//        public string Figi { get; }
+//    }
+//}
